64d3b6bb38fc49f21644f74880543f3dc10e1fcd
kotono.ukai <helloiamktn@gmail.com>
2021-12-31 20:32:56 +0900

    ごはんですよ一応動くようになった

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { db } from "./firebase";
+import gohan1 from "./images/gohan1.png";
+import gohan2 from "./images/gohan2.png";
+import gohan3 from "./images/gohan3.png";
+import gohan4 from "./images/gohan4.png";
+import gohan5 from "./images/gohan5.png";
+import gohan6 from "./images/gohan6.png";
+import togu from "./images/togu.png";
 
 function App() {
+  const [stock, setStock] = useState(10);
+
+  const docRef = db.collection("stock").doc("stock");
+  useEffect(() => {
+    docRef
+      .get()
+      .then((doc: any) => {
+        if (!doc) return;
+        setStock(doc.data().stock);
+      })
+      .catch((error: any) => {
+        console.log("Error getting document:", error);
+      });
+  }, []);
+
+  const remove = () => {
+    if (stock > 0) {
+      setStock((prev) => prev - 1);
+      docRef.set({
+        stock: stock - 1,
+      });
+    }
+  };
+
+  const add = () => {
+    setStock((prev) => prev + 1);
+    docRef.set({
+      stock: stock + 1,
+    });
+  };
+  const image = (stock: number) => {
+    switch (stock) {
+      case 0:
+        return togu;
+      case 1:
+        return gohan1;
+      case 2:
+        return gohan2;
+      case 3:
+        return gohan3;
+      case 4:
+        return gohan4;
+      case 5:
+        return gohan5;
+      default:
+        return gohan6;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p className='text'>
+        ごはんの残りは
+        <br />
+        あと{stock}個だよ
+      </p>
+      <div className='buttons'>
+        <Button variant='outlined' onClick={remove} className='button'>
+          食べた
+        </Button>
+        <Button variant='outlined' onClick={add} className='button'>
+          炊いた
+        </Button>
+      </div>
+      <div className='background'>
+        <img src={image(stock)} className='gohan' alt='ごはん' />
+      </div>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA50K98gR_YybPar5tdJ6KiiOrSobRjrfQ",
+  authDomain: "gohan-stock.firebaseapp.com",
+  projectId: "gohan-stock",
+  storageBucket: "gohan-stock.appspot.com",
+  messagingSenderId: "718659793898",
+  appId: "1:718659793898:web:f08e1e514c5303ad107893",
+  measurementId: "G-107882GSRH",
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
+export const db = firebase.firestore();
